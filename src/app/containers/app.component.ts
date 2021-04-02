44ca830b2892328a146496a108b793c11d3e05e3
Will Davis <will.davis@mtna.us>
2021-04-02 08:33:37 -0400

    feat(tabs): add compacted json view

--- src/app/containers/app.component.ts
+++ src/app/containers/app.component.ts
@@ -3,9 +3,10 @@ import { Component, HostBinding, OnInit, ViewEncapsulation } from '@angular/core
 import { MatIconRegistry } from '@angular/material/icon';
 import { TdDialogService } from '@covalent/core/dialogs';
 import { JsonLdArray } from 'jsonld/jsonld-spec';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, forkJoin } from 'rxjs';
 
 import { ThemeType } from '../components';
+import { GenericResource } from '../models';
 import { MockApiService } from '../services';
 
 @Component({
@@ -20,6 +21,9 @@ export class AppComponent implements OnInit {
     return this.activeTheme === 'LIGHT';
   }
 
+  compacted$ = new BehaviorSubject<GenericResource | null | undefined>(
+    undefined
+  );
   expanded$ = new BehaviorSubject<JsonLdArray | null | undefined>(undefined);
   loading$ = new BehaviorSubject<boolean>(false);
 
@@ -41,38 +45,40 @@ export class AppComponent implements OnInit {
 
   displayResource(formValue: { type: string; id: string }): void {
     this.loading$.next(true);
-    this.mockApiService
-      .expandResource(`${formValue.type}${formValue.id}`)
-      .subscribe({
-        next: (expanded) => this.expanded$.next(expanded),
-        error: (error: HttpErrorResponse) => {
-          this.expanded$.next(null);
-          this.loading$.next(false);
-          console.error(error);
-          if (error.status === 0) {
-            this.dialogService.openAlert({
-              title: 'Server Error',
-              disableClose: true,
-              message:
-                'The server is not responding. Are you sure the Ariā API is running at "http://localhost:8080/aria-api/api"?',
-            });
-          } else {
-            this.dialogService.openAlert({
-              title: `${error.error.code} Error`,
-              disableClose: true,
-              message: error.error.errors?.join(','),
-            });
-          }
-        },
-        complete: () => this.loading$.next(false),
-      });
+    forkJoin([
+      this.mockApiService.getResource(`${formValue.type}${formValue.id}`),
+      this.mockApiService.expandResource(`${formValue.type}${formValue.id}`),
+    ]).subscribe({
+      next: ([compacted, expanded]) => {
+        this.compacted$.next(compacted);
+        this.expanded$.next(expanded);
+      },
+      error: (error: HttpErrorResponse) => {
+        this.compacted$.next(null);
+        this.expanded$.next(null);
+        this.loading$.next(false);
+        console.error(error);
+        if (error.status === 0) {
+          this.dialogService.openAlert({
+            title: 'Server Error',
+            disableClose: true,
+            message:
+              'The server is not responding. Are you sure the Ariā API is running at "http://localhost:8080/aria-api/api"?',
+          });
+        } else {
+          this.dialogService.openAlert({
+            title: `${error.error.code} Error`,
+            disableClose: true,
+            message: error.error.errors?.join(','),
+          });
+        }
+      },
+      complete: () => this.loading$.next(false),
+    });
   }
 
   ngOnInit(): void {
     // Display the initially set resource
     this.displayResource(this.initialResource);
-
-    // TODO dynamic UI display
-    // TODO resolve terms
   }
 }
